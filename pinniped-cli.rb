d1790219fa912b7fb3644d2aa1af9fafd0be8db5
Pinny <pinniped-ci-bot@users.noreply.github.com>
2026-01-30 20:30:31 +0000

    pinniped-cli.rb: update to 0.44.0

--- pinniped-cli.rb
+++ pinniped-cli.rb
@@ -2,8 +2,8 @@ class PinnipedCli < Formula
     desc "Pinniped CLI for Kubernetes cluster authentication"
     homepage "https://pinniped.dev/"
     url "https://github.com/vmware-tanzu/pinniped.git",
-        tag:      "v0.43.0",
-        revision: "ff71d7e7680ebaa9e9d1c3c61f1a99bb205eaa85"
+        tag:      "v0.44.0",
+        revision: "eef4934775fbcb8508931736a32a144541fc7323"
     license "Apache-2.0"
     head "https://github.com/vmware-tanzu/pinniped.git", branch: "main"
     
